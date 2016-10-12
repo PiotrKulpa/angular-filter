a6b0e7b6420b2d8c80d4dac51c02aec47c9e277e
Piotr Kulpa <p.kulpa@onet.eu>
2016-10-12 22:04:11 +0200

    Commit no. 3

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,17 @@
  * Controller of the angularFilterApp
  */
 angular.module('angularFilterApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('scripts/database.json')
+    .then(function (response) {$scope.names = response.data.records;});
+    $scope.myCategory = {
+      'Id ascending' : {dbValue : 'id'},
+      'Id descending' : {dbValue : '-id'},
+      'Title ascending' : {dbValue : 'title'},
+      'Title descending' : {dbValue : '-title'},
+      'Time ascending' : {dbValue : 'date'},
+      'Time descending' : {dbValue : '-date'}
+    }
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
