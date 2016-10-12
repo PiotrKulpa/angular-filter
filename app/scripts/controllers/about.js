cab1e84251c9b5cee9f2b5187ee150f84683234e
Piotr Kulpa <p.kulpa@onet.eu>
2016-10-12 17:44:56 +0200

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFilterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularFilterApp
+ */
+angular.module('angularFilterApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFilterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularFilterApp
+ */
+angular.module('angularFilterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
